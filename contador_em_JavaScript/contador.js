004049d07f7f1ed2ca673cf99188b41da4830e00
David Mclaurel Brito Tavares <davidmac9@hotmail.com>
2023-04-19 20:19:01 -0300

    são apenas atualizações após mudanças no meu notebook
    são apenas atualizações após mudanças no meu notebook

---
+++ contador_em_JavaScript/contador.js
@@ -0,0 +1,37 @@
+window.addEventListener("load",() => {
+      const botao = document.getElementById("botao");
+
+      botao.addEventListener("click",() => {
+       // window.addEventListener("load", () => {
+
+//tempo em segundos,que queremos.
+let sec = 180;
+
+
+const countDiv = document.getElementById("timer");
+
+const secpass = () => {
+  let min = Math.floor(sec / 60);
+  let segundosRestantes = sec % 60;
+
+  if (segundosRestantes < 10) {
+    segundosRestantes = "0" + segundosRestantes;
+  }
+
+  if (min < 10) {
+    min = "0" + min;
+  }
+
+  countDiv.innerHTML = min + ":" + segundosRestantes;
+
+  // Condicao final
+  if (sec > 0) {
+    sec = sec - 1;
+  } else {
+    countDiv.innerHTML = "Acabou o tempo !";
+  }
+};
+
+const countDown = setInterval(() => secpass(), 1000);
+});
+})
